0554588bf4349817fbf4c7ab426e1c4041215061
Faustino Galceran <faustinogalceran@gmail.com>
2022-03-18 04:53:26 -0300

    intentando utilizar librerias

--- proyecto/script.js
+++ proyecto/script.js
@@ -21,3 +21,17 @@ formContacto.addEventListener('submit', (e) => {
     console.log(Contacto)
     
 })
+Toastify({
+    text: "Sus datos fueron enviados correctamente.",
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "left", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: Contacto(){} // Callback after click
+  }).showToast();
